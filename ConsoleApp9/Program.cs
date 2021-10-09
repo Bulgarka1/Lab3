dc96e782c93c84ed9cd97311124039e49ab9400f
Ксения Чемлекчиева <your_next_life@mail.ru>
2021-10-09 14:57:55 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+//y = {  5x − 2, x ≥ 2.5   3 + x, − 250 < x < 2.5  2x + 1, x ≤ −250
