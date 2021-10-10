c936cef47b14055532de5c7c11eb59b844ab3c41
Ксения Чемлекчиева <your_next_life@mail.ru>
2021-10-10 14:09:45 +0300

    написала код

--- ConsoleApp9/Program.cs
+++ ConsoleApp9/Program.cs
@@ -1,12 +1,85 @@
 ﻿using System;
 
-namespace ConsoleApp9
+namespace Lab3_2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool x, y, z;
+            x = true;
+            y = true;
+            z = true;
+            if(f(x,y,z)==true)
+            {
+                Console.WriteLine("x={0}, y={1}, z={2}, f(x,y,z)={3}", x, y, z, f(x, y, z));
+                Console.Read();
+                return;
+
+            }
+            x = false;
+            y = true;
+            z = true;
+            if(f(x,y,z)==true)
+            {
+                Console.WriteLine("x={0}, y={1}, z={2}, f(x,y,z)={3}", x, y, z, f(x, y, z));
+                Console.ReadLine();
+                return;
+            }
+            x = true;
+            y = false;
+            z = true;
+            if (f(x, y, z) == true) 
+            {
+                Console.WriteLine("x={0}, y={1}, z={2}, f(x,y,z)={3}", x, y, z, f(x, y, z));
+                Console.ReadLine();
+                return;
+            }
+            x = true;
+            y = true;
+            z = false;
+            if (f(x, y, z) == true) 
+            {
+                Console.WriteLine("x={0}, y={1}, z={2}, f(x,y,z)={3}", x, y, z, f(x, y, z));
+                Console.ReadLine();
+                return;
+            }
+            x = true;
+            y = false;
+            z = false;
+            if (f(x, y, z) == true) 
+            {
+                Console.WriteLine("x={0}, y={1}, z={2}, f(x,y,z)={3}", x, y, z, f(x, y, z));
+                Console.ReadLine();
+                return;
+            }
+            x = false;
+            y = false;
+            z = true;
+            if (f(x, y, z) == true) 
+            {
+                Console.WriteLine("x={0}, y={1}, z={2}, f(x,y,z)={3}", x, y, z, f(x, y, z));
+                Console.ReadLine();
+                return;
+            }
+            x = false;
+            y = true;
+            z = true;
+            if (f(x, y, z) == true) 
+            {
+                Console.WriteLine("x={0}, y={1}, z={2}, f(x,y,z)={3}", x, y, z, f(x, y, z));
+                Console.ReadLine();
+                return;
+            }
+
+            Console.WriteLine("f(x,y,z)=true, such values of x,y,z not found");
+            Console.ReadLine();
+        }
+
+        static bool f(bool x, bool y, bool z)
+        {
+            return !y && x && z && !z;
         }
     }
 }
+//16) f(x, y, z) = !y&x&z&!z = true
